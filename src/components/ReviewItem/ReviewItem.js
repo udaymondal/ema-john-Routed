54dc06abba7984e0e751780d6fa69f52e352d27b
udaymondal <udaysk.mondal@gmail.com>
2021-03-11 01:44:04 +0600

    removeBAKI

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -3,10 +3,18 @@ import React from 'react';
 const ReviewItem = (props) => {
     console.log(props);
     const { name, quantity} = props.product;
+    const reviewItemStyle={
+        borderBottom: '1px solid lightgray',
+        marginBottom: '5px',
+        paddingBottom: '15px',
+        marginLeft: '200px'
+    };
     return (
-        <div>
-            <h4>{name}</h4>
-            <p>{quantity}</p>
+        <div style={reviewItemStyle}>
+            <h4 className='product-name'>{name}</h4>
+            <p>Quantity: {quantity}</p>
+            <br/>
+            <button className='main-button'>Remove</button>
         </div>
     );
 };
